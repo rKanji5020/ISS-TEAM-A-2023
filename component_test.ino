74bc3318b1e381e5bba896ebaff8bf89376a88c0
rKanji5020 <116040694+rKanji5020@users.noreply.github.com>
2023-12-04 17:54:49 -0800

    Update component_test.ino

--- component_test.ino
+++ component_test.ino
@@ -1,35 +1,45 @@
+
+#define IO7 (13)              // Input/Output to payload plus 
+#define IO6 (12)              // Input/Output to payload plus 
+#define IO5 (11)              // Input/Output to payload plus
+#define IO4 (10)              // Input/Output to payload plus
+#define IO3 (9)               // Input/Output to payload plus
+#define IO2 (4)               // Input/Output to payload plus
+#define IO1 (3)               // Input/Output to payload plus
+#define IO0 (A6)              // Input/Output to payload plus 
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(0, OUTPUT);  //nutrient pump pin1
-  pinMode(1, OUTPUT);  //antibiotic pump pin2
+  pinMode(IO1, OUTPUT);  //nutrient pump pin1
+  pinMode(IO2, OUTPUT);  //antibiotic pump pin2
   //camera leds
-  pinMode(4, OUTPUT); // WHITE LED pin3
-  pinMode(5, OUTPUT); // BLUE LED 2 pin4
+  pinMode(IO5, OUTPUT); // WHITE LED pin3
+  pinMode(IO6, OUTPUT); // BLUE LED 2 pin4
   Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  digitalWrite(0, HIGH);
-  digitalWrite(1, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
+  digitalWrite(IO1, HIGH);
+  digitalWrite(IO2, HIGH);
+  digitalWrite(IO5, HIGH);
+  digitalWrite(IO6, HIGH);
 
-  delay(5000);
-  digitalWrite(0, LOW);
-  digitalWrite(1, LOW);
+  delay(2000);
+  digitalWrite(IO1, LOW);
+  //digitalWrite(IO2, LOW);
 
-  delay(5000);
-  digitalWrite(0, HIGH);
-  digitalWrite(1, HIGH);
+  delay(2000);
+  digitalWrite(IO1, HIGH);
+  //digitalWrite(IO2, HIGH);
 
-  delay(5000);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
+  //delay(2000);
+  //digitalWrite(IO5, LOW);
+  //digitalWrite(IO6, LOW);
 
-  delay(5000);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
+  //delay(2000);
+  //digitalWrite(IO5, HIGH);
+  //digitalWrite(IO6, HIGH);
   
 }
